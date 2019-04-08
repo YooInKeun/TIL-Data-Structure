fee6d4846587509893bfbda6ebafb58133709e45
Yoo In Keun <keun0390@naver.com>
2019-04-08 14:24:13 +0900

    다항식(insertNode 구현)
    Polynomial(insertNode implementation)

--- Linked List/Polynomial(doubly linked list).c
+++ Linked List/Polynomial(doubly linked list).c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct polyNode { // Polynomial Node 구현
@@ -11,29 +11,35 @@ typedef struct polyNode { // Polynomial Node 구현
 }polyNode;
 
 polyNode* creatNode(float coef, int exp); // Node 생성
-void addNode(polyNode** head, polyNode* newNode); // Node 추가
-void printList(polyNode** head); // Linked List 출력
+void insertNode(polyNode** firstNode, polyNode* newNode); // Node 삽입(항의 Exponent 값에 맞게 알맞은 위치에 삽입)
+void printList(polyNode** firstNode); // Linked List 출력
 
 int main() {
 
 	polyNode* A = NULL;
 	polyNode* B = NULL;
 
-	// A = 3x^2 - 5x^1 + 6
-
-	addNode(&A, creatNode(3, 2));
-	addNode(&A, creatNode(-5, 1));
-	addNode(&A, creatNode(6, 0));
+	insertNode(&A, creatNode(3, 2));
+	insertNode(&A, creatNode(-5, 1));
+	insertNode(&A, creatNode(6, 0));
+	insertNode(&A, creatNode(5, 5));
+	insertNode(&A, creatNode(-10, 4));
+	insertNode(&A, creatNode(50, 0));
+	insertNode(&A, creatNode(15, 5));
 
 	printf("A : ");
 	printList(&A);
 	printf("\n");
 
-	// B = -7x^3 + 3x^2 - 5
-
-	addNode(&B, creatNode(-7, 3));
-	addNode(&B, creatNode(3, 2));
-	addNode(&B, creatNode(-5, 0));
+	insertNode(&B, creatNode(-7, 3));
+	insertNode(&B, creatNode(3, 1));
+	insertNode(&B, creatNode(5, 3));
+	insertNode(&B, creatNode(-5, 0));
+	insertNode(&B, creatNode(10, 6));
+	insertNode(&B, creatNode(3, 2));
+	insertNode(&B, creatNode(-5, 0));
+	insertNode(&B, creatNode(10, 5));
+	insertNode(&B, creatNode(6, 2));
 
 	printf("B : ");
 	printList(&B);
@@ -45,6 +51,7 @@ int main() {
 polyNode* creatNode(float coef, int exp) { // Node 생성
 
 	polyNode* newNode = (polyNode*)malloc(sizeof(polyNode)); // Node 동적할당
+
 	newNode->coef = coef;
 	newNode->exp = exp;
 	newNode->nextLink = NULL;
@@ -53,33 +60,67 @@ polyNode* creatNode(float coef, int exp) { // Node 생성
 	return newNode;
 }
 
-void addNode(polyNode** head, polyNode* newNode) { // Node 추가
+void insertNode(polyNode** firstNode, polyNode* newNode) { // Node 삽입(항의 Exponent 값에 맞게 알맞은 위치에 삽입)
 
-	if (*head == NULL) { // List에 Node가 없으면 head가 첫 번째 newNode를 가리킴
+	if (*firstNode == NULL) { // List에 Node가 없으면 firstNode가 첫 번째 newNode를 가리킴
 
-		*head = newNode;
+		*firstNode = newNode;
 	}
 
 	else { // List에 Node가 있으면
 
-		polyNode* tail = *head;
+		polyNode* nodePtr = *firstNode;
 
-		while (tail->nextLink != NULL) { // head가 가리키는 첫 번째 Node부터 순차적으로 탐색해서 마지막 Node 발견 시
+		// Case 3가지
+		// 1. newNode가 맨 앞에 insert 되는 경우
+		// 2. newNode가 중간에 insert 되는 경우
+		// 3. newNode가 맨 뒤에 insert 되는 경우
 
-			tail = tail->nextLink;
+		int myCase = 1; // newNode가 맨 앞에 insert 되는 경우
+
+		while (nodePtr->exp > newNode->exp) { // nodePtr이 가리키는 Node의 Exponent 값과 newNode의 Exponent 값 비교
+
+			if (nodePtr->nextLink != NULL) { // nodePtr의 nextLink가 NULL이 아니면, newNode가 중간에 insert 되는 경우
+
+				nodePtr = nodePtr->nextLink;
+				myCase = 2;
+			}
+
+			else { // nodePtr의 nextLink가 NULL이면, newNode가 맨 뒤에 insert 되는 경우
+
+				myCase = 3;
+				break;
+			}
 		}
 
-		tail->nextLink = newNode; // 마지막 Node의 nextLink는 추가된 Node를 가리킴
-		newNode->nextLink = NULL; // 새로 추가된 Node의 nextLink는 NULL 값
-		newNode->prevLink = tail; // 새로 추가된 Node의 prevLink는 이전의 마지막 Node를 가리킴
+		if (myCase == 1) { // newNode가 맨 앞에 insert
+
+			*firstNode = newNode;
+			newNode->nextLink = nodePtr;
+			nodePtr->prevLink = newNode;
+		}
+
+		else if (myCase == 2) { // newNode가 중간에 insert
+
+			newNode->nextLink = nodePtr;
+			newNode->prevLink = nodePtr->prevLink;
+			nodePtr->prevLink->nextLink = newNode;
+			nodePtr->prevLink = newNode;
+		}
+
+		else if(myCase == 3) { // newNode가 마지막에 insert
+
+			nodePtr->nextLink = newNode;
+			newNode->prevLink = nodePtr;
+		}
 	}
 }
 
-void printList(polyNode** head) { // Linked List 출력
+void printList(polyNode** firstNode) { // Linked List 출력
 
-	polyNode* current = *head;
+	polyNode* current = *firstNode;
 	float myCoef; // 계수의 부호를 판별
-	int myExp;
+	int myExp; // 지수가 0인지 아닌지를 판별
 
 	while (current != NULL) {
 
@@ -96,7 +137,6 @@ void printList(polyNode** head) { // Linked List 출력
 			else { // 지수가 0이 아니면
 
 				printf("%.2fx^%d", myCoef, myExp); // 계수와 지수 값 모두 출력
-
 			}
 		}
 
@@ -110,7 +150,6 @@ void printList(polyNode** head) { // Linked List 출력
 			else if (current->nextLink->coef < 0) { //  // 만약 다음 항의 부호가 - 면, + 기호를 출력할 필요가 없음
 
 				printf(" ");
-
 			}
 		}
 
