4d67ad85b4baaf8869b7b6a5a9033d62e63f1509
Yoo In Keun <keun0390@naver.com>
2019-04-10 15:28:04 +0900

    다항식의 합 구현
    Polynomial Addtion

--- Linked List/Polynomial(doubly linked list).c
+++ Linked List/Polynomial(doubly linked list).c
@@ -10,45 +10,47 @@ typedef struct polyNode { // Polynomial Node 구현
 
 }polyNode;
 
-polyNode* creatNode(float coef, int exp); // Node 생성
+polyNode* createNode(float coef, int exp); // Node 생성
 void insertNode(polyNode** firstNode, polyNode* newNode); // Node 삽입(항의 Exponent 값에 맞게 알맞은 위치에 삽입)
 void printList(polyNode** firstNode); // Linked List 출력
+
+polyNode* Addition(polyNode** poly1, polyNode** poly2); // 두 개의 Polynomial의 합
 
 int main() {
 
 	polyNode* A = NULL;
 	polyNode* B = NULL;
 
-	insertNode(&A, creatNode(3, 2));
-	insertNode(&A, creatNode(-5, 1));
-	insertNode(&A, creatNode(6, 0));
-	insertNode(&A, creatNode(5, 5));
-	insertNode(&A, creatNode(-10, 4));
-	insertNode(&A, creatNode(50, 0));
-	insertNode(&A, creatNode(15, 5));
+	insertNode(&A, createNode(3, 2));
+	insertNode(&A, createNode(-5, 1));
+	insertNode(&A, createNode(4, 2));
+	insertNode(&A, createNode(4, 2));
+	insertNode(&A, createNode(6, 3));
+	insertNode(&A, createNode(5, 0));
+	insertNode(&A, createNode(3, 5));
 
 	printf("A : ");
 	printList(&A);
-	printf("\n");
 
-	insertNode(&B, creatNode(-7, 3));
-	insertNode(&B, creatNode(3, 1));
-	insertNode(&B, creatNode(5, 3));
-	insertNode(&B, creatNode(-5, 0));
-	insertNode(&B, creatNode(10, 6));
-	insertNode(&B, creatNode(3, 2));
-	insertNode(&B, creatNode(-5, 0));
-	insertNode(&B, creatNode(10, 5));
-	insertNode(&B, creatNode(6, 2));
+	insertNode(&B, createNode(-7, 3));
+	insertNode(&B, createNode(6, 1));
+	insertNode(&B, createNode(1, 2));
+	insertNode(&B, createNode(5, 2));
+	insertNode(&B, createNode(-9, 0));
 
 	printf("B : ");
 	printList(&B);
-	printf("\n");
+
+	polyNode* C = NULL; // C = A + B
+	C = Addition(&A, &B);
+
+	printf("Polynomial Addition : ");
+	printList(&C);
 
 	return 0;
 }
 
-polyNode* creatNode(float coef, int exp) { // Node 생성
+polyNode* createNode(float coef, int exp) { // Node 생성
 
 	polyNode* newNode = (polyNode*)malloc(sizeof(polyNode)); // Node 동적할당
 
@@ -71,24 +73,52 @@ void insertNode(polyNode** firstNode, polyNode* newNode) { // Node 삽입(항의
 
 		polyNode* nodePtr = *firstNode;
 
-		// Case 3가지
-		// 1. newNode가 맨 앞에 insert 되는 경우
-		// 2. newNode가 중간에 insert 되는 경우
-		// 3. newNode가 맨 뒤에 insert 되는 경우
+		/*
+
+		< Case 4가지 >
+
+		List에 newNode의 Exponent와 동일한 Node가 존재하지 않을 때
+
+		[Case 1] newNode가 맨 앞에 insert 되는 경우
+		[Case 2] newNode가 중간에 insert 되는 경우
+		[Case 3] newNode가 맨 뒤에 insert 되는 경우
+
+		List에 newNode의 Exponent와 동일한 Node가 존재할 때
+
+		[Case 4] insert 하는 경우, 기존의 항을 기존의 항과 newNode의 합으로 저장
+
+		(insert 삽입을 진행하고, 보다 깔끔한 식을 위해 차수가 같은 항끼리는 더하기 진행)
+
+		*/
 
 		int myCase = 1; // newNode가 맨 앞에 insert 되는 경우
 
-		while (nodePtr->exp > newNode->exp) { // nodePtr이 가리키는 Node의 Exponent 값과 newNode의 Exponent 값 비교
+		while (nodePtr->exp >= newNode->exp) { // nodePtr이 가리키는 Node의 Exponent 값과 newNode의 Exponent 값 비교
 
-			if (nodePtr->nextLink != NULL) { // nodePtr의 nextLink가 NULL이 아니면, newNode가 중간에 insert 되는 경우
+			if (nodePtr->nextLink != NULL) { // nodePtr의 nextLink가 NULL이 아니면
 
-				nodePtr = nodePtr->nextLink;
-				myCase = 2;
+				if (nodePtr->exp == newNode->exp) { // Exponenent 값이  같을 경우
+
+					myCase = 4; // Case 4
+					break;
+				}
+
+				else { // Exponenent 값이  같지 않고, newNode가 중간에 insert 되는 경우
+
+					nodePtr = nodePtr->nextLink;
+					myCase = 2; // Case 2
+				}
 			}
 
-			else { // nodePtr의 nextLink가 NULL이면, newNode가 맨 뒤에 insert 되는 경우
+			else if (nodePtr->nextLink == NULL) { // nodePtr의 nextLink가 NULL이면, newNode가 맨 뒤에 insert 되는 경우
 
-				myCase = 3;
+				if (nodePtr->exp == newNode->exp) { // Exponenent 값이  같을 경우
+
+					myCase = 4; // Case 4
+					break;
+				}
+
+				myCase = 3; // Case 3
 				break;
 			}
 		}
@@ -108,10 +138,16 @@ void insertNode(polyNode** firstNode, polyNode* newNode) { // Node 삽입(항의
 			nodePtr->prevLink = newNode;
 		}
 
-		else if(myCase == 3) { // newNode가 마지막에 insert
+		else if (myCase == 3) { // newNode가 마지막에 insert
 
 			nodePtr->nextLink = newNode;
 			newNode->prevLink = nodePtr;
+		}
+
+		else if (myCase == 4) { // 기존의 항을 기존의 항과 newNode의 합으로 저장
+
+			nodePtr->coef += newNode->coef;
+			free(newNode); // newNode 메모리 해제
 		}
 	}
 }
@@ -147,7 +183,7 @@ void printList(polyNode** firstNode) { // Linked List 출력
 				printf(" +");
 			}
 
-			else if (current->nextLink->coef < 0) { //  // 만약 다음 항의 부호가 - 면, + 기호를 출력할 필요가 없음
+			else if (current->nextLink->coef < 0) { // 만약 다음 항의 부호가 - 면, + 기호를 출력할 필요가 없음
 
 				printf(" ");
 			}
@@ -156,5 +192,80 @@ void printList(polyNode** firstNode) { // Linked List 출력
 		current = current->nextLink;
 	}
 
-	printf("\n");
+	printf("\n\n");
+}
+
+polyNode* Addition(polyNode** poly1, polyNode** poly2) { // 두 개의 Polynomial의 합
+
+	polyNode* nodePtr1 = *poly1;
+	polyNode* nodePtr2 = *poly2;
+
+	polyNode* myList = NULL; // poly1과 poly2의 합을 담을 List
+	int myCase;
+
+	while (!(nodePtr1 == NULL || nodePtr2 == NULL)) { // nodePtr1과 nodePtr2 둘 중에 하나라도 NULL이 될 때까지 더하기 진행
+
+		if (nodePtr1->exp > nodePtr2->exp) {
+
+			insertNode(&myList, createNode(nodePtr1->coef, nodePtr1->exp));
+		}
+
+		else if (nodePtr1->exp == nodePtr2->exp) {
+
+			insertNode(&myList, createNode(nodePtr1->coef + nodePtr2->coef, nodePtr1->exp));
+			nodePtr2 = nodePtr2->nextLink;
+		}
+
+		else if (nodePtr1->exp < nodePtr2->exp) {
+
+			insertNode(&myList, createNode(nodePtr2->coef, nodePtr2->exp));
+			nodePtr2 = nodePtr2->nextLink;
+		}
+
+		nodePtr1 = nodePtr1->nextLink;
+
+		/*
+
+		< Case 2가지 >
+
+		[Case 1] 다항식 poly1의 차수가 다항식 poly2의 차수보다 클 때
+		[Case 2] 다항식 poly2의 차수가 다항식 poly1의 차수보다 클 때
+
+		*/
+
+		if (nodePtr1 == NULL) {
+
+			myCase = 1;
+		}
+
+		else if (nodePtr2 == NULL) {
+
+			myCase = 2;
+		}
+
+		else {
+
+			myCase = 3;
+		}
+	}
+
+	if (myCase == 1) {
+
+		while (!(nodePtr2 == NULL)) {
+
+			insertNode(&myList, createNode(nodePtr2->coef, nodePtr2->exp));
+			nodePtr2 = nodePtr2->nextLink;
+		}
+	}
+
+	else if (myCase == 2) {
+
+		while (!(nodePtr1 == NULL)) {
+
+			insertNode(&myList, createNode(nodePtr1->coef, nodePtr1->exp));
+			nodePtr1 = nodePtr1->nextLink;
+		}
+	}
+
+	return myList;
 }
